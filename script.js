4e62acc88538df9ea4fe6c55f3717b96dacde5e9
Devu <devudevu10@gmail.com>
2024-03-30 19:45:05 +1100

    data manipulations

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // variables
 
 // var, let and const
-
+/*
 var name = "devendra raj dahal";
 
 console.log(name);
@@ -28,3 +28,12 @@ const a = 3;
 const b = 4;
 c = a ** b;
 console.log(c);
+*/
+
+// const answer = Math;
+// console.log(answer);
+
+// console.log(typeof "03465465");
+
+var a = undefined;
+console.log(typeof a, a);
