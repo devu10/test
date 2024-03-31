cb1db38ebf867bf2a767f709b2e213d89d9e55ed
Devu <devudevu10@gmail.com>
2024-03-31 22:46:08 +1100

    key events mapped

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    window.location.href = "events.html";
+  }
+  if (e.key === "Escape") {
+    alert("escape cooming soon..");
+  }
+});
