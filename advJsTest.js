a04042b82cc62fbbc8d2f8e84811ffbbe1549c6f
Devu <devudevu10@gmail.com>
2024-04-17 21:48:41 +1000

    closure

--- advJsTest.js
+++ advJsTest.js
@@ -6,10 +6,41 @@ function outerFunScope() {
   function innerFunScope() {
     var innerFuncVar = "inner";
 
-    console.log(innerFunScope);
+    console.log(innerFuncVar);
     console.log(outerFunVar);
     console.log(globalVar);
   }
 }
 
-innerFunScope();
+const makeCounter = function () {
+  let privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment() {
+      changeBy(1);
+    },
+
+    decrement() {
+      changeBy(-1);
+    },
+
+    value() {
+      return privateCounter;
+    },
+  };
+};
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+console.log(counter1.value()); // 0.
+
+counter1.increment();
+counter1.increment();
+console.log(counter1.value()); // 2.
+
+counter1.decrement();
+console.log(counter1.value()); // 1.
+console.log(counter2.value()); // 0.
