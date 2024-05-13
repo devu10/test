ee4aaffdfb2046b864f6b76c25cbc917b1ad818f
Devu <devudevu10@gmail.com>
2024-05-13 23:12:31 +1000

    csv test cleared

--- csvtest.js
+++ csvtest.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const csvString = document.getElementById("txt");
-  const displayCsvString = document.getElementById("dis");
-  const submitForm = document.getElementById("subbtn");
+const csvString = document.getElementById("txt");
+const displayCsvString = document.getElementById("dis");
+const submitForm = document.getElementById("subbtn");
+const clearBtn = document.querySelector("#clrbtn");
 
-  submitForm.addEventListener("click", (e) => {
-    const inp = csvString.value;
-    console.log(inp);
-    displayCsvString.innerText = inp;
-  });
+let inputArr = "";
+let input = "";
+
+submitForm.addEventListener("click", () => {
+  input = csvString.value;
+
+  inputArr = input.split("\n");
+
+  displayCsvString.innerText = inputArr;
+});
+
+clearBtn.addEventListener("click", () => {
+  csvString.value = "";
+  displayCsvString.innerText = "";
 });
